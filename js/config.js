abef93145098426e575d6e3bc56236de95b3e106
Open Systems <OpenSystemsConsulting@users.noreply.github.com>
2016-02-08 15:00:08 +1030

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ angular.module('config', [])
 // appConfig object contains app specific data which will NOT normally change between clients
 // e.g. the log server will normally come to OSC so we can monitor for errors etc.
 .constant('appConfig', {
-	'version':			299.32,
+	'version':			77.32,
 	'logServerIP':		'opensyscon.com.au',
 	'logServerPort':	5678
 })
