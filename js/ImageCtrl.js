b6612e3830d9b1201700fc7a9454b96d5b8c5263
root <root@wpedb.opensyscon.com.au>
2017-04-05 14:56:05 +0000

    New pda param for testing images

--- js/ImageCtrl.js
+++ js/ImageCtrl.js
@@ -1,11 +1,10 @@
 'use strict';
 
 angular.module('ImageCtrl', [])
-	.controller('ImageCtrl', [ '$rootScope', '$scope', 'imageService','imageFileService','fileUpload','$cordovaFileTransfer','$cordovaFile','pda_images','$window','$ionicPopup','pdaParams',
-		function ($rootScope, $scope, imageService, imageFileService, fileUpload, $cordovaFileTransfer, $cordovaFile, pda_images, $window, $ionicPopup, pdaParams) {
+	.controller('ImageCtrl', [ '$rootScope', '$scope', 'imageService','imageFileService','fileUpload','$cordovaFileTransfer','$cordovaFile','pda_images','$window','$ionicPopup',
+		function ($rootScope, $scope, imageService, imageFileService, fileUpload, $cordovaFileTransfer, $cordovaFile, pda_images, $window, $ionicPopup) {
 
-		var lpda_images = pdaParams.pda_images ? 'Y' : 'N';;
-		if(pda_images != 'Y' && lpda_images != 'Y') {
+		if(pda_images != 'Y') {
 			var alertPopup = $ionicPopup.alert({
 				title: 'Module not installed',
 				template: 'This module is not installed.'
