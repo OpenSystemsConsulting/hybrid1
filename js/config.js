ce8eff2b148bec32abd733fea983482f266bfc04
root <root@oscvtest.opensyscon.com.au>
2018-09-24 12:19:48 +1000

    updated version to 2.58.48

--- js/config.js
+++ js/config.js
@@ -12,8 +12,8 @@ angular.module('config', [])
 // appConfig object contains app specific data which will NOT normally change between clients
 // e.g. the log server will normally come to OSC so we can monitor for errors etc.
 .constant('appConfig', {
-	'version':			'2.58.47',
-	'build':			3,
+	'version':			'2.58.48',
+	'build':			1,
 	'logServerIP':		'opensyscon.com.au',
 	'logServerPort':	5678
 })
