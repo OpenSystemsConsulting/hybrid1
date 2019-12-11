c62f9a073c066c047ef3132f27175b810a297895
Open Systems Consulting <osc.supp@gmail.com>
2019-12-11 14:51:22 +1100

    commitMsg

--- build/main.js
+++ build/main.js
@@ -14685,7 +14685,7 @@ var ConfigServiceProvider = /** @class */ (function () {
         //IMPORTANT Note: if appConfig.version < 2.35, then push notifications won't work properly.
         this.appConfig = {
             'version': '3.1.14',
-            'build': 24,
+            'build': 25,
             'logServerIP': 'opensyscon.com.au',
             'logServerPort': 5678
         };
